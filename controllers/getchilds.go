3ebdba93f67fd1068c51c4ab194bb64f19fa12a3
Jitiwattana <bbos1@windowslive.com>
2019-05-26 00:33:19 +0700

    ADD get left child with recursive

--- controllers/getchilds.go
+++ controllers/getchilds.go
@@ -1,21 +1,31 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/babyjazz/demo/db"
-	"github.com/babyjazz/demo/models"
+	"github.com/go-pg/pg"
 	"github.com/labstack/echo"
 )
 
+type Users struct {
+	Id           int               `json:"id,omitempty"`
+	Username     string            `json:"username,omitempty"`
+	Fullname     string            `json:"fullname,omitempty"`
+	ChildLeftId  int               `json:"child_left_id,omitempty"`
+	ChildRightId int               `json:"child_right_id,omitempty"`
+	Ok           map[string]*Users `json:",omitempty" sql:"-"`
+}
+
 func GetChilds(c echo.Context) (err error) {
-	type Response struct {
-		Success bool          `json:"success"`
-		Data    *models.Users `json:"data,omitempty"`
-		Message string        `json:"message,omitempty"`
-	}
 	type Request struct {
 		Username string `json:"username"`
+	}
+	type Response struct {
+		Success bool   `json:"success"`
+		Data    *Users `json:"data,omitempty"`
+		Message string `json:"message,omitempty"`
 	}
 
 	req := new(Request)
@@ -26,16 +36,15 @@ func GetChilds(c echo.Context) (err error) {
 	pgdb := db.Connect()
 	defer pgdb.Close()
 
-	userModel := new(models.Users)
+	// Set parent
+	userModel := new(Users)
 
 	err = pgdb.Model(userModel).Where("username=?", req.Username).First()
 	if err != nil {
-		res := &Response{
-			Success: false,
-			Message: "Username is invalid",
-		}
-		return c.JSON(http.StatusUnauthorized, res)
+		fmt.Println("User is not found")
 	}
+	userModel.Ok = make(map[string]*Users)
+	userModel.Ok["child"], err = getChild(req.Username, pgdb)
 
 	res := &Response{
 		Success: true,
@@ -43,4 +52,24 @@ func GetChilds(c echo.Context) (err error) {
 	}
 
 	return c.JSON(http.StatusOK, res)
+}
+
+func getChild(username string, pgdb *pg.DB) (user *Users, err error) {
+	a := new(Users)
+	s := new(Users)
+
+	err = pgdb.Model(a).Where("username=?", username).First()
+	if err != nil {
+		fmt.Println("User is not found")
+	}
+	err = pgdb.Model(s).Where("id=?", a.ChildLeftId).First()
+	if err != nil {
+		fmt.Println("User is not found")
+	}
+	if s.ChildLeftId != 0 {
+		s.Ok = make(map[string]*Users)
+		s.Ok["child"], err = getChild(s.Username, pgdb)
+
+	}
+	return s, err
 }
